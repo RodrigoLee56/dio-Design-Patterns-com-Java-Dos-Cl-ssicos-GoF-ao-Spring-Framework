910c06f86d4ce9fd90f9bf890d353c0e1c62dba1
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-05-06 01:35:07 -0400

    feat: cria a classe Cliente

---
+++ lab-padroes-projeto-spring/src/main/java/com/desafio/projeto/lab_padroes_projeto_spring/model/Cliente.java
@@ -0,0 +1,45 @@
+package com.desafio.projeto.lab_padroes_projeto_spring.model;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+
+	@ManyToAny
+	private Endereco endereco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
